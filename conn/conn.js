35f6ece39d57ec5d8a9bbe57ee335ead33cc969b
khamankartukesh <tukeshkhamankar@gmail.com>
2025-11-02 18:25:28 +0530

    Update backend configuration and dependencies

--- conn/conn.js
+++ conn/conn.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const conn = async () => {
     try {
-        await mongoose.connect("mongodb+srv://todo_user:uCswZVIsiMAcNbMK@todo.wwjm17f.mongodb.net/?appName=todo");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ Database Connected");
     } catch (error) {
-        console.log("❌ Database connection failed", error);
+        console.error("❌ Database connection failed:", error);
         process.exit(1);
     }
 };
